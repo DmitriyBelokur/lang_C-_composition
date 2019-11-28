e26307f31c16a579a73ac74fecc38836296352a1
Mykola Solyanko <mykola_solianko@epam.com>
2019-11-28 15:58:10 +0200

    Change logic some topics

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,7 +1,12 @@
-#include <stdio.h>
+/*
+ подключаем в нашу программу библиотечный файл
+ для того чтобы использовать std::cout
+*/
+#include <iostream>
 // g++ basic_program.cpp -o basic_program
 int main(int argc, char const *argv[])
 {
-  printf("First program \n");
+  // печатаем на консоль сообщение
+  std::cout << "First program \n";
   return 0;
 }
--- literals.cpp
+++ literals.cpp
@@ -1,8 +1,14 @@
+/*
+ подключаем в нашу программу специальные заголовочные файлы,
+ для того что бы использовать  printf, std::cout, std::endl
+*/
+
 #include <stdio.h>
 #include <iostream>
 // g++ -Wall -std=c++1y literals.cpp -o literals
 int main(int argc, char const *argv[])
 {
+  // используеться С подход для печати литералов на консоль
   printf("This is integer literal %d\n", 10);
   printf("This is integer literal in hex format 0xA =  %d\n", 0xA);
   printf("This is integer literal represent in octal format %o\n", 10);
@@ -11,6 +17,13 @@ int main(int argc, char const *argv[])
   printf("This is char literal %c\n", 'c');
   printf("This is string literal %s\n", "string");
 
+  // используеться С++ подход для печати литералов на консоль
+  std::cout << "My first string print to cout" << std::endl;
+  std::cout << "Float literal " << 1.25 << std::endl;
+  /*
+   указываем то что мы хотим выводить на консоль true и false
+   если это не указать то мы получим на консоле 0 и 1
+  */
   std::cout << std::boolalpha;
   std::cout << true << " " << false << std::endl;
   return 0;
